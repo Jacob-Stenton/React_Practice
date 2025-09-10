ae07cfb65d5e6144edf0e3984102b6e055b4480a
Jacob <j.stenton0420211@arts.ac.uk>
2025-09-10 17:44:42 +0100

    openClose alert

--- React_Practice1/src/App.tsx
+++ React_Practice1/src/App.tsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
+import Alert from "./components/Alert";
 import Button from "./components/Button";
 
 function App() {
+  const [alertVisable, setAlertVisable] = useState(false);
+
   return (
     <div>
-      <Button color="danger" onClick={() => console.log("click")}>
+      {alertVisable && (
+        <Alert onClose={() => setAlertVisable(false)}>My Alert</Alert>
+      )}
+      <Button color="danger" onClick={() => setAlertVisable(true)}>
         Hello World
       </Button>
     </div>
